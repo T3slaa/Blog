31ae9bb78d0c83f29259b617fc64ebce10ffe200
T369 <codex_150@protonmail.com>
2022-02-26 23:23:12 -0300

    Change avatar

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220224.2312';
+const cacheName = 'chirpy-20220225.1528';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
