e3669fe58805a7adb2cf37b15cb2607364691f38
T369 <codex_150@protonmail.com>
2022-07-06 16:16:21 -0400

    RouterSpace HackTheBox :bento: :chart_with_upwards_trend:

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220603.2205';
+const cacheName = 'chirpy-20220706.1554';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
